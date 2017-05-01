084fc106ddede82c6115f9f4988372f19211f318
Dos Santos Pita Ruben <dossantoru@etmlnet.local>
2017-05-01 10:07:05 +0200

    [BUGFIX] Fixed user creation script

--- user pour la bd.sql
+++ user pour la bd.sql
@@ -1,3 +1,3 @@
-DROP USER 'userclass'@'localhost';
+DROP USER IF EXISTS 'userclass'@'localhost';
 CREATE USER 'userclass'@'localhost' IDENTIFIED BY 'managepass';
 GRANT ALL PRIVILEGES ON db_manageclass.* TO 'userclass'@'localhost' WITH GRANT OPTION;
